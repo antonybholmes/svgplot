9dc94027a8f3fc36d6511bbad5124a945c4956e1
Antony Holmes <antony@antonyholmes.dev>
2025-07-24 13:53:35 -0400

    fix: Updated font dimension calc in core

--- svgplot/core.py
+++ svgplot/core.py
@@ -5,11 +5,11 @@ Created on Fri Mar  1 17:40:38 2019
 @author: antony
 """
 
-#import matplotlib
+# import matplotlib
 # matplotlib.use('agg')
-#import matplotlib.pyplot as plt
-#import numpy as np
-#import pandas as pd
+# import matplotlib.pyplot as plt
+# import numpy as np
+# import pandas as pd
 
 from typing import Any, Mapping, Optional
 import matplotlib
@@ -19,18 +19,18 @@ import os
 from PIL import Image
 from svgwrite import cm, mm
 import sys
-from PIL import ImageFont
+from PIL import Image, ImageDraw, ImageFont
 import re
 import tkinter as Tkinter
 import tkinter.font as tkFont
- 
-L1 = 's'
-L2 = '^'
-L3 = 'v'
 
-DEFAULT_FONT_FAMILY = 'Arial'
+L1 = "s"
+L2 = "^"
+L3 = "v"
+
+DEFAULT_FONT_FAMILY = "Arial"
 DEFAULT_FONT_SIZE = 10
-DEFAULT_FONT_WEIGHT = 'normal'
+DEFAULT_FONT_WEIGHT = "normal"
 
 HEADING_FONT_SIZE = 14
 
@@ -51,17 +51,17 @@ SEP_CLUST_OFFSET = 20
 
 DEFAULT_IMAGE_SIZE = 300
 
-STROKE_SIZE = 2 #2
+STROKE_SIZE = 2  # 2
 
 
-COLORBAR_DIR = '/Users/Antony/OneDrive/Documents/work/shared/cumc/katia/manuscript/components/colorbar'
-UMAP_DIR = '/ifs/scratch/cancer/Lab_RDF/abh2138/scRNA/data/samples/human/10X/rdf/restricted_grch38/manuscript/components/umap'
-MONOCLE_DIR = '/ifs/scratch/cancer/Lab_RDF/abh2138/scRNA/data/samples/human/10X/rdf/restricted_grch38/manuscript/components/monocle'
-IF_DIR = '/ifs/scratch/cancer/Lab_RDF/abh2138/scRNA/data/samples/human/10X/rdf/restricted_grch38/manuscript/components/if'
-IF_PAIRS_DIR = '/ifs/scratch/cancer/Lab_RDF/abh2138/scRNA/data/samples/human/10X/rdf/restricted_grch38/manuscript/components/if/pairs'
+COLORBAR_DIR = "/Users/Antony/OneDrive/Documents/work/shared/cumc/katia/manuscript/components/colorbar"
+UMAP_DIR = "/ifs/scratch/cancer/Lab_RDF/abh2138/scRNA/data/samples/human/10X/rdf/restricted_grch38/manuscript/components/umap"
+MONOCLE_DIR = "/ifs/scratch/cancer/Lab_RDF/abh2138/scRNA/data/samples/human/10X/rdf/restricted_grch38/manuscript/components/monocle"
+IF_DIR = "/ifs/scratch/cancer/Lab_RDF/abh2138/scRNA/data/samples/human/10X/rdf/restricted_grch38/manuscript/components/if"
+IF_PAIRS_DIR = "/ifs/scratch/cancer/Lab_RDF/abh2138/scRNA/data/samples/human/10X/rdf/restricted_grch38/manuscript/components/if/pairs"
 
-LABEL_S_G2_M = 'S+G2+M'
-LABEL_G0_G1 = 'G0+G1'
+LABEL_S_G2_M = "S+G2+M"
+LABEL_G0_G1 = "G0+G1"
 
 PADDING = 10
 LEGEND_TEXT_OFFSET = 10
@@ -70,23 +70,23 @@ LEGEND_GROUP_OFFSET = LEGEND_BRACKET_OFFSET + 10
 BRACKET_SIZE = 15
 
 
-COLOR_WHITE = 'white'
-COLOR_BLACK = 'black'
-NONE = 'none'
+COLOR_WHITE = "white"
+COLOR_BLACK = "black"
+NONE = "none"
 
 # mm per pt to get sizing
-FONT_SCALE_FACTOR = 254 / 72 #3.52777763324264 #3.54166641  # 1 #0.35432 3.51852 #
+FONT_SCALE_FACTOR = 254 / 72  # 3.52777763324264 #3.54166641  # 1 #0.35432 3.51852 #
 
 AXIS_LENGTH = 100
 
 ARROW_LENGTH = 90
 
-#CLUSTER_COLORS = figures.ClusterColors()
+# CLUSTER_COLORS = figures.ClusterColors()
 
 LABEL_COLOR_BLOCK_SIZE = 12
 
 GRID_STROKE = STROKE_SIZE
-GRID_COLOR = 'gainsboro' #'Gainsboro'
+GRID_COLOR = "gainsboro"  #'Gainsboro'
 TICK_SIZE = 10
 MINOR_TICK_SIZE = TICK_SIZE - 4
 
@@ -96,53 +96,72 @@ MINOR_TICK_STROKE = 2
 
 # initialize Tk so that font metrics will work
 
-def kws(kws: dict[str, Any], user_kws: Optional[dict[str, Any]] = None) -> dict[str, Any]:
+
+def kws(
+    kws: dict[str, Any], user_kws: Optional[dict[str, Any]] = None
+) -> dict[str, Any]:
     if user_kws is not None:
         kws.update(user_kws)
     return kws
 
-def clamp(x): 
+
+def clamp(x):
     return max(0, min(int(x * 255), 255))
 
+
 def rgbtohex(rgba):
-    
-    return '#{:02x}{:02x}{:02x}'.format(clamp(rgba[0]), clamp(rgba[1]), clamp(rgba[2]))
+
+    return "#{:02x}{:02x}{:02x}".format(clamp(rgba[0]), clamp(rgba[1]), clamp(rgba[2]))
+
 
 def rgbatohex(rgba):
-    return '#{:02x}{:02x}{:02x}'.format(clamp(rgba[0]), clamp(rgba[1]), clamp(rgba[2]), clamp(rgba[3]))
+    return "#{:02x}{:02x}{:02x}".format(
+        clamp(rgba[0]), clamp(rgba[1]), clamp(rgba[2]), clamp(rgba[3])
+    )
+
 
 def hexcolortrans(hexcolor, trans=0.5):
-    return '{}{:02x}'.format(hexcolor, clamp(trans))
+    return "{}{:02x}".format(hexcolor, clamp(trans))
 
 
-
-ABC_COLORS = {'ABC':'red', 'GCB':'green', 'Unclass':'blue'}
-
-
-CMAP = matplotlib.cm.get_cmap('tab10', 10)
-SHIPP_COLORS = {'C1':CMAP(0), 'C2':CMAP(1), 'C3':CMAP(2), 'C4':CMAP(3), 'C5':CMAP(4), 'Unclassified':'gray'}
-
-DHIT_COLORS = {'DHITsig-pos':rgbatohex(matplotlib.cm.get_cmap('tab20c')(16)), 
-               'DHITsig-neg':rgbatohex(matplotlib.cm.get_cmap('tab20c')(18)),
-               'n/a': 'none'} # 'lightgray'{'DHITsig-pos':'dimgray', 'DHITsig-neg':'silver'}
-
-SURVIVAL_Y_LABEL = 'PFS'
-OS_SURVIVAL_Y_LABEL = 'OS'
+ABC_COLORS = {"ABC": "red", "GCB": "green", "Unclass": "blue"}
 
 
-def css_param(name:str, value:str) -> str:
+CMAP = matplotlib.cm.get_cmap("tab10", 10)
+SHIPP_COLORS = {
+    "C1": CMAP(0),
+    "C2": CMAP(1),
+    "C3": CMAP(2),
+    "C4": CMAP(3),
+    "C5": CMAP(4),
+    "Unclassified": "gray",
+}
+
+DHIT_COLORS = {
+    "DHITsig-pos": rgbatohex(matplotlib.cm.get_cmap("tab20c")(16)),
+    "DHITsig-neg": rgbatohex(matplotlib.cm.get_cmap("tab20c")(18)),
+    "n/a": "none",
+}  # 'lightgray'{'DHITsig-pos':'dimgray', 'DHITsig-neg':'silver'}
+
+SURVIVAL_Y_LABEL = "PFS"
+OS_SURVIVAL_Y_LABEL = "OS"
+
+
+def css_param(name: str, value: str) -> str:
     # if isinstance(value, str) and value != 'none' and re.search('\d+', value) is None:
     #    value = "'{}'".format(value)
 
-    return '{}: {}'.format(name, value)
+    return "{}: {}".format(name, value)
 
 
 def css_params(*params):
-    return '; '.join([css_param(params[i], params[i + 1]) for i in range(0, len(params), 2)])
+    return "; ".join(
+        [css_param(params[i], params[i + 1]) for i in range(0, len(params), 2)]
+    )
 
 
-def format_css_params(params:Mapping[str, str]) -> str:
-    return '; '.join([css_param(param, params[param]) for param in sorted(params)])
+def format_css_params(params: Mapping[str, str]) -> str:
+    return "; ".join([css_param(param, params[param]) for param in sorted(params)])
 
 
 def img_dim(f):
@@ -197,10 +216,10 @@ def scaled_image_w(f, h):
     return round(iw * r, 3)
 
 
-def find_gene_exp_dir(group, colormap='bgy'):
+def find_gene_exp_dir(group, colormap="bgy"):
     dir = find_umap_dir(group)
 
-    dir = os.path.join(dir, 'GeneExp', colormap, 'trimmed')
+    dir = os.path.join(dir, "GeneExp", colormap, "trimmed")
 
     return dir
 
@@ -210,92 +229,111 @@ def find_gene_exp_file(group, c):
 
     for n in os.listdir(dir):
         d = os.path.join(dir, n)
-        if c in d and 'png' in d:
+        if c in d and "png" in d:
             return d
 
     return None
 
 
 def find_exp_file(name, dir):
-    #dir = '{}/GeneExp/trimmed'.format(dir)
+    # dir = '{}/GeneExp/trimmed'.format(dir)
 
     files = os.listdir(dir)
-    
+
     matches = []
-    
+
     for file in files:
-        if name in file and 'trimmed' in file and 'BGY' not in file:
+        if name in file and "trimmed" in file and "BGY" not in file:
             matches.append(os.path.join(dir, file))
-            
+
     ret = None
-    
+
     for file in sorted(matches):
-        if file.endswith('jpg'):
+        if file.endswith("jpg"):
             ret = file
             break
-        
+
     if ret is None:
         for file in sorted(matches):
-            if file.endswith('png'):
+            if file.endswith("png"):
                 ret = file
                 break
-        
-    print('Found exp file', ret)
+
+    print("Found exp file", ret)
 
     return ret
 
 
-def find_sep_clust_files(name, dir, method='umap'):
+def find_sep_clust_files(name, dir, method="umap"):
     ret = []
 
     files = os.listdir(dir)
 
     for file in sorted(files):
         print(file, name)
-        if '_{}_'.format(name) in file and 'trimmed' in file and method in file and 'sep_clust' in file and 'png' in file:
+        if (
+            "_{}_".format(name) in file
+            and "trimmed" in file
+            and method in file
+            and "sep_clust" in file
+            and "png" in file
+        ):
             ret.append(os.path.join(dir, file))
 
     return ret
 
 
-def get_text_metrics(text,
-                     size=DEFAULT_FONT_SIZE,
-                     family=DEFAULT_FONT_FAMILY,
-                     weight=DEFAULT_FONT_WEIGHT):
-    #font = tkFont.Font(family=family, size=size, weight=weight)
-    #(w, h) = (font.measure(text), font.metrics('linespace'))
+def get_text_metrics(
+    text, size=DEFAULT_FONT_SIZE, family=DEFAULT_FONT_FAMILY, weight=DEFAULT_FONT_WEIGHT
+):
+    # font = tkFont.Font(family=family, size=size, weight=weight)
+    # (w, h) = (font.measure(text), font.metrics('linespace'))
     # return (w, h)
 
-    family = family  + '.ttf'
+    family = family + ".ttf"
 
-#    f = ImageFont.truetype(family, size)
-#
-#    w = 0
-#    h = 0
-#
-#    for c in text:
-#        s = f.getsize(c)
-#        w += s[0]
-#        h = max(h, s[1])
+    #    f = ImageFont.truetype(family, size)
+    #
+    #    w = 0
+    #    h = 0
+    #
+    #    for c in text:
+    #        s = f.getsize(c)
+    #        w += s[0]
+    #        h = max(h, s[1])
 
     # Gets spacing with 1 wrong
-    #text = text.replace('1', '8')
-    
-    t2 = ''
-    
-    for l in text:
-        if re.match(r'[A-Z0-9]', l):
-            t2 += 'A'
-        else:
-            t2 += 'a'
+    # text = text.replace('1', '8')
 
-    text = text.replace('c', 'a')
+    t2 = ""
+
+    for l in text:
+        if re.match(r"[A-Z0-9]", l):
+            t2 += "A"
+        else:
+            t2 += "a"
+
+    text = text.replace("c", "a")
     print(family)
     imf = ImageFont.truetype(family, size)
-    s = imf.getsize(text)
-    w = np.sum([imf.getsize(c)[0] for c in text])
-    
-    return [s[0], s[1]] #ImageFont.truetype(family, size).getsize(text) #''.join(['a'] * len(text))) #text.upper())
+    # s = imf.getsize(text)
+    # w = np.sum([imf.getsize(c)[0] for c in text])
+    img = Image.new("RGB", (1000, 1000))
+    draw = ImageDraw.Draw(img)
+
+    bbox = draw.textbbox((0, 0), text, font=imf)  # returns (x0, y0, x1, y1)
+    text_width = bbox[2] - bbox[0]
+    text_height = bbox[3] - bbox[1]
+
+    s = (text_width, text_height)
+
+    return [
+        s[0],
+        s[1],
+    ]
+
+    # ImageFont.truetype(family, size).getsize(text) #''.join(['a'] * len(text))) #text.upper())
+
 
 #    tk_root = Tkinter.Tk()
 #    key = (family, size, weight)
@@ -310,23 +348,23 @@ def format_rotate(rot):
         rot = 0
 
     if isinstance(rot, tuple) or isinstance(rot, list):
-        return 'rotate({} {} {})'.format(rot[0], rot[1], rot[2])
+        return "rotate({} {} {})".format(rot[0], rot[1], rot[2])
     elif isinstance(rot, int) or isinstance(rot, float):
-        return 'rotate({} 0 0)'.format(rot)
+        return "rotate({} 0 0)".format(rot)
     else:
-        return 'rotate(0 0 0)'
+        return "rotate(0 0 0)"
 
 
 def format_translate(x, y):
-    return 'translate({}, {})'.format(round(x, 3), round(y, 3))
+    return "translate({}, {})".format(round(x, 3), round(y, 3))
 
 
 def rot_trans(rotate=0, x=0, y=0):
-    return '{} {}'.format(format_rotate(rotate), format_translate(x, y))
+    return "{} {}".format(format_rotate(rotate), format_translate(x, y))
 
 
 def scale_viewbox(w, h, vw=10):
-    return '0 0 {} {}'.format(vw, round(vw * h / w, 3))
+    return "0 0 {} {}".format(vw, round(vw * h / w, 3))
 
 
 def find_dir(dir, name):
@@ -343,10 +381,10 @@ def find_umap_dir(name):
 
 
 def find_umap_png(dir, name):
-    print('find', name)
+    print("find", name)
     for f in os.listdir(dir):
         print(f)
-        if 'umap' in f and 'png' in f and name in f:
+        if "umap" in f and "png" in f and name in f:
             print(f, name)
             return os.path.join(dir, f)
 
@@ -360,38 +398,39 @@ def find_monocle_dir(name):
 def find_monocle_png(dir):
     for f in os.listdir(dir):
         f = os.path.join(dir, f)
-        if 'ptime' in f and 'edges' not in f and 'png' in f:
+        if "ptime" in f and "edges" not in f and "png" in f:
             return f
 
     return None
 
 
-def find_sep_clust_dir(group, type='umap'):
-    if type == 'monocle':
+def find_sep_clust_dir(group, type="umap"):
+    if type == "monocle":
         dir = find_monocle_dir(group)
     else:
         dir = find_umap_dir(group)
 
-    dir = os.path.join(dir, 'sep_clust', 'trimmed')
+    dir = os.path.join(dir, "sep_clust", "trimmed")
 
     return dir
 
 
-def find_sep_clust_file(group, c, type='umap'):
+def find_sep_clust_file(group, c, type="umap"):
     dir = find_sep_clust_dir(group, type=type)
 
     for n in os.listdir(dir):
         f = os.path.join(dir, n)
-        if ('c{}.'.format(c) in f or 'c{}_'.format(c) in f) and 'png' in f:
+        if ("c{}.".format(c) in f or "c{}_".format(c) in f) and "png" in f:
             return f
 
     return None
 
 
-def find_if_dir(name, sub=''):
+def find_if_dir(name, sub=""):
     return os.path.join(find_dir(IF_DIR, name), sub)
 
-def find_if_file(group, name, sub=''):
+
+def find_if_file(group, name, sub=""):
     dir = find_if_dir(group, sub=sub)
 
     nl = name.lower()
@@ -403,10 +442,12 @@ def find_if_file(group, name, sub=''):
 
     return None
 
-def find_if_pairs_dir(name, sub=''):
+
+def find_if_pairs_dir(name, sub=""):
     return os.path.join(find_dir(IF_PAIRS_DIR, name), sub)
 
-def find_if_pairs_file(group, name, sub=''):
+
+def find_if_pairs_file(group, name, sub=""):
     dir = find_if_pairs_dir(group, sub=sub)
 
     nl = name.lower()
